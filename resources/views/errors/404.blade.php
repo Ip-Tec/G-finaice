9d412465a9faaff97f66aec293a347d5216c15ee
innocent peter <otakhorpeter@gmail.com>
2024-02-11 07:26:28 +0100

    404 page

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Page Not Found - 404</title>
+</head>
+<body>
+    <h1>Oops! That page doesn't exist.</h1>
+    <p>It looks like you're trying to access a page that doesn't exist. Don't worry, it happens to the best of us.</p>
+    <a href="{{ route('welcome') }}">Home</a>
+</body>
+</html>
